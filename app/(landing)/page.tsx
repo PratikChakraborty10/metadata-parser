9e8ce6829bc16b248822e9fdfd61b4ef33b683d7
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2024-02-22 02:10:21 +0530

    initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>Header Space</div>
+  )
+}
+
+export default LandingPage
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Meta Data Parser",
+  description: "An application to preview all the valid meta data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
