bd546cf83fbd947953a8d8bd07670f874dba0cd8
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2024-02-29 02:08:25 +0530

    some changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,22 @@
-import Mainabar from '@/components/mainbar'
+"use client"
+
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 function LandingPage() {
+  const [searchTerm, setSearchTerm] = useState("")
+  const router = useRouter()
+  const search = () => {
+    router.push("/results")
+  }
   return (
-    <div>
-      
+    <div className='h-full w-full flex justify-center items-center'>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="Enter website url" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+      <Button type="submit" onClick={search}>Parse</Button>
+    </div>
     </div>
   )
 }
